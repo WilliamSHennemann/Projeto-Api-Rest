70e91c3b7694de35f38ccf4bae95ffe857996353
William S.H. <williamdossantoshennemann@gmail.com>
2026-05-11 20:04:28 -0300

    Arrumando erros
    Arrumando problemas nos arquivos

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Conectado: ${conn.connection.host}`);
+    console.log(`MongoDB Conectado com sucesso`);
   } catch (error) {
     console.error(`Erro ao conectar MongoDB: ${error.message}`);
     process.exit(1);
